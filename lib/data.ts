2241b7ad20be640dcd9fe7b552e1e5171c0dcfc7
Humansh-lab <herotech45@gmail.com>
2026-03-25 13:15:34 +0530

    fix: added missing dependencies

--- lib/data.ts
+++ lib/data.ts
@@ -29,16 +29,16 @@ export const team: TeamMember[] = [
   {
     name: "Humansh",
     role: "Founder",
-    image: "/team/you.png",
+    image: "/team/you.jpeg",
   },
   {
     name: "shiv",
-    role: "Editor",
+    role: "Manager",
     image: "/team/member1.png",
   },
   {
     name: "sachin",
-    role: "Designer",
+    role: "Editor",
     image: "/team/member2.png",
   },
   {
@@ -48,7 +48,7 @@ export const team: TeamMember[] = [
   },
   {
     name: "mohit",
-    role: "Manager",
+    role: "Designer",
     image: "/team/member4.png",
   },
 ]
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   turbopack: {
-    root: './fox-studio', // path to your actual project root
+    root: __dirname, // absolute path fix
   },
 };
 
